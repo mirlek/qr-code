9cdf49eae27d7e1e52135bba0700d08ecb25cbe7
mirlek <mirlenko.ekat@gmail.com>
2023-09-14 21:17:38 +0200

    QR Code Component is implemented

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "../src/images/image-qr-code.png";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Improve your front-end skills by building projects</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Scan the QR code to visit Frontend Mentor and take your coding skills
+          to the next level
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
